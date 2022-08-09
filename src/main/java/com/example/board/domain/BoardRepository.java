7da4b2681fc58ee3cef83cd6f6dc27521ca5d00f
woals990605 <woals990605@naver.com>
2022-08-09 19:22:10 +0900

    비밀번호 확인후 삭제하기중

--- src/main/java/com/example/board/domain/BoardRepository.java
+++ src/main/java/com/example/board/domain/BoardRepository.java
@@ -1,7 +1,13 @@
 package com.example.board.domain;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface BoardRepository extends JpaRepository<Board, Integer>{
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface BoardRepository extends JpaRepository<Board, Integer> {
+
+    @Query(value = "SELECT * FROM Board WHERE password = :password", nativeQuery = true)
+    Optional<Board> checkPassword(@Param("password") Board board);
 }
